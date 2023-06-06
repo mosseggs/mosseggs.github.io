bfae5a2751e89748357132d0fbf97883bb0ed7ab
mosseggs <114590430+mosseggs@users.noreply.github.com>
2023-06-06 19:55:40 +0000

    some commit

---
+++ mosseggs.github.io/bullet.js
@@ -0,0 +1,26 @@
+class Bullet{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.dy = 11;
+        this.diameter = 10 * 1.5;
+    }
+    display(){
+        fill(50,50,255);
+        circle(this.x,this.y,this.diameter);
+    }
+    update(){
+        this.y-=this.dy;
+    }
+    collides(object){
+        if (dist(this.x, this.y, object.x, object.y) < ((this.diameter/2) + (object.diameter/2)))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+ }
+
